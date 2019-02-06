342103df1e324503d00deb2681a498eef7614ea7
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2019-02-06 13:03:54 -0800

    added a changeIngridient function

--- src/App.js
+++ src/App.js
@@ -72,6 +72,36 @@ class App extends Component {
         this.setState(state);
     };
 
+    changeIngredient = (name, factor) => {
+        // скопировать ингредиент
+        let ingredient = {...this.state.ingredients[name]};
+
+
+        let price = availableIngredients.find(item => item.name === name).price;
+        if (factor<0 && ingredient.count==0){
+          ingredient.count = ingredient.count;
+        }
+        else {
+          ingredient.count += factor;
+        }
+        ingredient.total = ingredient.count * price;
+
+        // скопипровать объект "ингредиенты"
+        let ingredients = {...this.state.ingredients};
+
+        // поменять ингредиент в копии объекта "ингредиенты"
+        ingredients[name] = ingredient;
+
+        // скопировать состояние (state) компонента App.js
+        let state = {...this.state};
+
+        // поменять объект "игредиенты" в копии состояния (state)
+        state.ingredients = ingredients;
+
+        // задать новый state с перерисовкой компонентов на странице
+        this.setState(state);
+    };
+
     calculatePrice = () => {
       let price = 0
       for (let i in this.state.ingredients){
@@ -92,7 +122,7 @@ class App extends Component {
                 {/* чтобы получить и вывести результат. */}
                 {/* под ценой вывести форму BurgerForm */}
                 {/* в форме вывести IngredientControl для каждого ингредиента */}
-                <Form price={this.calculatePrice()} add={this.addIngredient} remove={this.removeIngredient}/>
+                <Form price={this.calculatePrice()} add={this.addIngredient} remove={this.changeIngredient}/>
             </div>
         );
     }
