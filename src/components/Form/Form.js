cbf8bd69edde4523c7f31f5016d3f32292209b3d
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2019-02-06 20:14:48 -0800

    changed the Row render in Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,11 +16,6 @@ function Form(props) {
           )
           }
       </div>
-
-    <Row name="salad" clickLess = {()=>{props.remove('salad')}} clickMore = {() => {props.add('salad')}}/>
-    <Row name="cheese" clickLess = {()=>{props.remove('cheese')}} clickMore = {() => {props.add('cheese')}}/>
-    <Row name="meat" clickLess = {()=>{props.remove('meat')}} clickMore = {() => {props.add('meat')}}/>
-    <Row name="bacon" clickLess = {()=>{props.remove('bacon')}} clickMore = {() => {props.add('bacon')}}/>
     <button type="button" className="btn btn-success" onClick={props.order}>Order</button>
   </div>);
 }
